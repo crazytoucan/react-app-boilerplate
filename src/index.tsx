c1d3f5a9e2dd5ed4c389c4c6944ac9218e393c0d
Alex Ryan <alexanderfryan@gmail.com>
2017-12-25 14:45:39 -0600

    Clarify HMR comment.

--- src/index.tsx
+++ src/index.tsx
@@ -19,16 +19,19 @@ document.addEventListener("DOMContentLoaded", () => {
 
 /*
  * The block below is needed for Hot Module Reloading (HMR) in webpack 3.
- * We need to re-`require` the file within this block, as otherwise the old version
- * of the code will be referenced in the reload (which is bad).
+ * We need to re-`require` the ./components/App file within this block,
+ * as otherwise the old version of the code will be referenced in the reload (which is bad).
  *
  * Webpack 3 hot reloading works by propagating any change you make up the `import`
- * structure in your code, so any code that hits the App.tsx file will trigger the
- * reload callback below. Furthermore, code changes that don't hit App.tsx won't
- * get caught by HMR.
+ * structure in your code, so any code that hits the App.tsx file (or any file it imports,
+ * transitively) will trigger the reload callback below.
+ * Furthermore, code changes that don't propagate up to App.tsx won't get caught by HMR.
  *
  * This code is obviously fickle. https://github.com/gaearon/react-hot-loader#known-limitations
  * walks through some gotchas for component registration.
+ *
+ * The below JavaScript is optimized out during the distributable build, as webpack
+ * will declare `module.hot: false` and shake the dead code.
  */
 declare const module: any;
 if (module.hot) {
